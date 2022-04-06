25aafc98d4582385d8916412d8f0eb7434f9fb47
+ <+>
2022-04-06 16:21:49 -0400

    commit tree d2acfec079476f35d1c975f330ac435d4e507144

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import CategoryFilter from "./CategoryFilter";
 import NewTaskForm from "./NewTaskForm";
 import TaskList from "./TaskList";
 
 import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
 
 function App() {
+  const [taskItems, setTaskItems] = useState(TASKS)
+  const [category, setCategory] = useState("All");
+  const onTaskFormSubmit = (newTask) => {
+    setTaskItems([...taskItems, newTask])
+  }
+
+  const deleteTask = (text) => {
+    setTaskItems(taskItems.filter(task => task.text !== text))
+  }
+
   return (
     <div className="App">
       <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
+      <CategoryFilter
+        categories={CATEGORIES}
+        setCategory={setCategory}
+      />
+      <NewTaskForm
+        categories={CATEGORIES.filter((catergory) => catergory !== "All")}
+        onTaskFormSubmit={onTaskFormSubmit}
+      />
+      <TaskList deleteTask={deleteTask} tasks={taskItems.filter(task => category === "All" || task.category === category)}/>
     </div>
   );
 }
--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 
-function CategoryFilter() {
+function CategoryFilter({categories, setCategory}) {
+  const [selected, setSelected] = useState("")
+  const handleClick = (e, category) => {
+    setSelected(e.target.textContent)
+    setCategory(category)
+  }
   return (
     <div className="categories">
       <h5>Category filters</h5>
-      {/* render <button> elements for each category here */}
+      {
+        categories.map((category, i) => {
+          return (
+            <button
+              key={i}
+              onClick={(e) => handleClick(e, category)}
+              className={selected === category ? 'selected' : null}
+            >
+              {category}
+            </button>
+          )
+        })
+      }
+      
     </div>
   );
 }
